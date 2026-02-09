26637f2f57e1eb293a2f805e8e6b666ea9b3bdc4
Gokul <gklgrme@gmail.com>
2026-02-09 13:38:30 +0530

    fix:github and google auth

--- constants/routeHandler.ts
+++ constants/routeHandler.ts
@@ -3,5 +3,9 @@
 
 
 export const NEXT_API_ROUTES = {
-    USERS_LIST_API:'/api/users'
+    USERS_LIST_API:'/api/users',
+
+    
+    GOOGLE_AUTH:'/api/auth/google-verify',
+    GITHUB_AUTH:'/api/auth/github-verify'
 }
